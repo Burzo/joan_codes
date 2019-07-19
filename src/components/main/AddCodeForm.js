13cf47abf77a5ab18064ad3efe200398391e079f
Burzo <miha.drofenik92@gmail.com>
2019-07-19 23:13:35 +0200

    added some styles

--- src/components/main/AddCodeForm.js
+++ src/components/main/AddCodeForm.js
@@ -9,6 +9,7 @@ class AddCodeForm extends React.Component {
 			newcodeInput: "",
 			suborpromo: "",
             selectValue: "",
+            isEURorUSD: "eur"
         }
     }
 
@@ -21,6 +22,14 @@ class AddCodeForm extends React.Component {
 	}
 	
 	radioChanged = (e) => {
+        switch (e.target.name) {
+            case "suborpromo":
+                this.setState({suborpromo: e.target.value})
+                break;
+            case "isEURorUSD":
+                    this.setState({isEURorUSD: e.target.value})
+                break
+        }
 		this.setState({suborpromo: e.target.value})
 	}
 
@@ -28,48 +37,59 @@ class AddCodeForm extends React.Component {
         return(
             <section className="section-addcodeform">
                 <form className="addcodeform" onSubmit={this.formSubmit}>
+
                     <div className="addcodeform__element">
                         <label className="addcodeform__element__label" htmlFor="newCode">
                             Enter code here:
-                            <input onChange={this.inputChanged} name="newCode" value={this.state.newcodeInput}/>
+                            <div className="addcodeform__inputcode">
+                                <input className="addcodeform__inputcode--label" onChange={this.inputChanged} name="newCode" value={this.state.newcodeInput}/>
+                            </div>
                         </label>
-                        <Tooltip>You can enter your own code here if you wish. Hopefully, it doesn't exists yet. Or just create a random one.</Tooltip>
+                        {/* <Tooltip>You can enter your own code here if you wish. Hopefully, it doesn't exists yet. Or just create a random one.</Tooltip> */}
                     </div>
                     <div className="addcodeform__element">
                         <label className="addcodeform__element__label" htmlFor="suborpromo">
 							Code type:
-							<div>
-								<label>
+							<div className="addcodeform__codetype">
+								<label className="addcodeform__codetype--radio">
 									<input onChange={this.radioChanged} checked={this.state.suborpromo === "sub"} name="suborpromo" value="sub" type="radio" />
 									Subscription code
 								</label>
-								<label>
+								<label className="addcodeform__codetype--radio">
 									<input onChange={this.radioChanged} checked={this.state.suborpromo === "promo"} name="suborpromo" value="promo" type="radio" />
 									Promotional code
 								</label>
 							</div>
+
                         </label>
-                        <Tooltip>Choose which type of code this is.<br/>Choose PROMO only if the customer didn't pay for it.</Tooltip>
+                        {/* <Tooltip>Choose which type of code this is.<br/>Choose PROMO only if the customer didn't pay for it.</Tooltip> */}
                     </div>
                     <div className="addcodeform__element">
                         <label className="addcodeform__element__label" htmlFor="suborpromo">
                             Code type:
-                            <select onChange={this.selectChanged} value={this.state.selectValue}>
+
+                            <select className="addcodeform__select" onChange={this.selectChanged} value={this.state.selectValue}>
                                 <option value="promo" type="checkbox">Promotional code</option>
                                 <option value="subscription" type="checkbox">Subscription code</option>
                             </select>
+
                         </label>
                     </div>
                     <div className="addcodeform__element">
-						<label className="addcodeform__toggle" htmlFor="eurorusd">
-							<span className="addcodeform__toggle--text">
-								€
-								<input value="eur" name="eurorusd" type="radio" />
-							</span>
-							<span className="addcodeform__toggle--text">
-								$
-								<input value="usd" name="eurorusd" type="radio" />
-							</span>
+						<label className="addcodeform__element__label">
+                            Currency:
+                            <div className="addcodeform__currency">
+                                <span className="addcodeform__currency__container">
+                                    <label className={"addcodeform__currency--label" + (this.state.isEURorUSD === "eur" ? "--selected" : "")}>
+                                        €
+                                        <input className="addcodeform__currency--radio" onClick={this.radioChanged} checked={this.state.isEURorUSD === "eur"} value="eur" name="isEURorUSD" type="radio" />
+                                    </label>
+                                    <label className={"addcodeform__currency--label" + (this.state.isEURorUSD === "usd" ? "--selected" : "")}>
+                                        $
+                                        <input className="addcodeform__currency--radio" onClick={this.radioChanged} checked={this.state.isEURorUSD === "usd"} value="usd" name="isEURorUSD" type="radio" />
+                                    </label>
+                                </span>
+                            </div>
 						</label>
                     </div>
                 </form>
